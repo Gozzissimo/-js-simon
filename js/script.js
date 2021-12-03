51645742a144949405d536dbce3746a9fc3e04dc
Gozzissimo <fantini.nico@gmail.com>
2021-12-03 16:22:36 +0100

    Funzione Prompt generator

--- js/script.js
+++ js/script.js
@@ -15,23 +15,25 @@
 // 1. generare un array con 5 numeri casuali (facciamo da uno a cento per es.) e metterli in pagina
 
 // Array
-const memoryArray = generateNumbers(1, 100, 5)
+const memoryArray = generateNumbers(1, 100, 5);
 // console.log(memoryArray);
 
 // facciamo apparire i numeri in pagina
-document.getElementById('memory-numbers').innerHTML = memoryArray
+let NumbersToMemory = document.getElementById('memory-numbers');
+NumbersToMemory.innerHTML = memoryArray;
 
 // 2.creare una funzione temporale che va da 0 a 30 secondi.
-setTimeout(() => {
-    
-}, 30000);
+// setTimeout(function() {
+//     NumbersToMemory.innerHTML = '';
+//     callingPrompts();
+// }, 30000);
 
 
 
 
 // FUNZIONI
 
-/**
+/** FUNZIONE PER GENERARE UN ARRAY DI NUMERI CASUALI
  * 
  * @param {*} min Numero Minimo
  * @param {*} max Numero Massimo
@@ -40,7 +42,7 @@ setTimeout(() => {
  */
 
 function generateNumbers(min, max, arrayLength) {
-    const numbers = []
+    const numbers = [];
 
     while (numbers.length < arrayLength) {
 
@@ -51,4 +53,30 @@ function generateNumbers(min, max, arrayLength) {
         }
     }
     return numbers;
+}
+
+
+/** FUNZIONE PER AVERE UN ARRAY CON x PROMPT PER INSERIRE DEI NUMERI
+ * 
+ * @param {*} arrayLength Numero di Prompt che volgio generare
+ * @returns Un array di Numeri
+ */
+
+function memoryPrompts(arrayLength) {
+    const promptNumbers = [];
+    
+
+    while (promptNumbers.length < arrayLength) {
+
+        let number = parseInt(prompt('Inserisci un numero tra quelli che hai visto'));
+
+        if (!promptNumbers.includes(number)) {
+            promptNumbers.push(number);
+        } else {
+            alert('Attenzione! questo numero è gia stato inserito');
+        }
+    
+    }
+
+    return promptNumbers
 }
